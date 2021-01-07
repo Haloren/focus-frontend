27f2110d57f1a700e936289b9c8898f3a017c90c
Haloren <mckennakyler@gmail.com>
2021-01-06 20:11:30 -0700

    added copyright footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import './App.css';
 
 let today = new Date();
 let dateToday = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
 
 class App extends React.Component {
+  
+  
+  
   render() {
     return (
       <>
         <h1>{dateToday}</h1>
+
+
+        <footer><small> &copy;2021 Mostly Focused </small></footer>
       </>
     );
   }
