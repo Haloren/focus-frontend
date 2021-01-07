c0a567203b442aafab3233d47de5d2456632c9a9
Haloren <mckennakyler@gmail.com>
2021-01-06 21:41:02 -0700

    set up Event form

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 class EventForm extends React.Component {
 
+    state = { }
+
     render() {
         return (
             <>
                 <h3>Add an Event</h3>
-                <input></input>
+                <form className="input-container" >
+                    <input type="text" name="event" placeholder="Enter Event Name (ex: Birth Anniversary)" required ></input>
+                    <input type="date" name="date" required ></input>
+                    <input type="submit" value="Add Event"></input>
+                </form>
             </>
         )
     }
