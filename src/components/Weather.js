467b02e7236250fb499073e61af246cbbab106b2
Haloren <mckennakyler@gmail.com>
2021-01-07 21:42:33 -0700

    set up css date time container

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,7 +5,7 @@ const Weather = (props) => {
     return (
         <>
             <h2> ⛅  88 ° </h2>
-            <h4> Anthem, AZ </h4>
+            <h5> Anthem, AZ </h5>
         </>
     )
 }
--- src/containers/DateTimeContainer.js
+++ src/containers/DateTimeContainer.js
@@ -4,9 +4,9 @@ import DateTime from '../components/DateTime';
 class DateTimeContainer extends React.Component {
     render() {
         return (
-            <>
+            <div className="date-time-container">
                 <DateTime />
-            </>
+            </div>
         )
     }
 }
