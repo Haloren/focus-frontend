218b69f4e5629a4f3b4089a70cc71aa0bb013898
Haloren <mckennakyler@gmail.com>
2021-01-07 18:49:38 -0700

    added notes for reducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 
 import todosReducer from './todosReducer'
+// import eventsReducer from './eventsReducer'
+// import usersReducer from './usersReducer'
 
 
 export const reducer = combineReducers({
     todos: todosReducer,
     // events: eventsReducer,
-    // user: userReducer
+    // users: usersReducer
 })
