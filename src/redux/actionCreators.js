5427f41c2b6789a5a1bec30d346afd47bce572e8
Haloren <mckennakyler@gmail.com>
2021-01-07 22:25:54 -0700

    set up addEvent post action

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,2 +1,33 @@
 const API = "http://localhost:3001/"
 
+//ADD DATA TO DATABASE
+export const addEvent = (event, userId) => {
+    return (dispatch) => {
+        fetch(API + "/events", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(event),
+        })
+        .then(resp => resp.json())
+        .then(event => {
+            if (event.message) {
+                alert(event.message)
+            } else {
+                dispatch({type: 'ADD_EVENT', payload: event})
+            }
+        })
+    }
+}
+
+export const addTodo = (todo, userId) => {
+    return (dispatch) => {
+        fetch(API + "/todos", {
+            method
+        })
+    }
+}
+
+// DELETE DATA FROM DATABASE
