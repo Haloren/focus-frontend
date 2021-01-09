987cdb70cca207693b23da0bd4e40217f22eec1b
Haloren <mckennakyler@gmail.com>
2021-01-08 20:04:33 -0700

    added reducers to combineReducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,14 @@
 import { combineReducers } from "redux";
 
 import usersReducer from './usersReducer'
-// import todosReducer from './todosReducer'
-// import eventsReducer from './eventsReducer'
-// import weatherReducer from './weatherReducer'
+import todosReducer from './todosReducer'
+import eventsReducer from './eventsReducer'
+import weatherReducer from './weatherReducer'
 
 
 export const reducer = combineReducers({
     users: usersReducer,
-    // todos: todosReducer,
-    // events: eventsReducer,
-    // weather: weatherReducer
+    todos: todosReducer,
+    events: eventsReducer,
+    weather: weatherReducer
 })
