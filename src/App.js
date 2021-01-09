29c7d87f8e096357eaeb9f3421fccff4a3d6c84c
Haloren <mckennakyler@gmail.com>
2021-01-08 21:58:41 -0700

    checking to see if a fetch request will work

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ import TodosContainer from './containers/TodosContainer';
 class App extends React.Component {
   
   componentDidMount() {
-    console.log(this)
+    fetch('http://localhost:3001/users')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
   }
   
   render() {
