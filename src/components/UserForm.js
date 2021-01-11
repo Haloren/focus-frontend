ffa7a2fd5dd427708ab5302f6ae6bb8fdf69b821
Haloren <mckennakyler@gmail.com>
2021-01-11 09:07:44 -0700

    fixed blank password issue

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -4,7 +4,7 @@ import { addUser } from '../redux/actionCreators'
 
 class UserForm extends React.Component {
 
-    state = { name: '', email: '', password: ''}
+    state = { name: '', email: '', password_digest: ''}
 
     handleOnChange = (event) => {
         this.setState({
@@ -15,7 +15,7 @@ class UserForm extends React.Component {
     handleOnSubmit = (event) => {
         event.preventDefault()
         this.props.addUser(this.state)
-        this.setState({name: '', email: '', password: ''});
+        this.setState({name: '', email: '', password_digest: ''});
     }
 
     render() {
@@ -24,7 +24,7 @@ class UserForm extends React.Component {
             <form onSubmit={this.handleOnSubmit}>
                 <input type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleOnChange} required></input>
                 <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleOnChange} required></input>
-                <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleOnChange} required></input>
+                <input type="password" name="password_digest" placeholder="password" value={this.state.password_digest} onChange={this.handleOnChange} required></input>
                 <input type="submit" value="Create Account"></input>
             </form>
         </>
