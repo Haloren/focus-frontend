ffba9f94158f877e65cb5d8886d2690b2e83c2b7
Haloren <mckennakyler@gmail.com>
2021-01-16 16:31:42 -0700

    added google font and general cleanup

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Clock from 'react-digital-clock';
 
 const DateTime = () => {
-    // DATE
     let today = new Date();
     let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
@@ -13,9 +12,9 @@ const DateTime = () => {
 
     return (
         <div className="date-time-container">
-            <h3>{day} {month} {date}, {year}</h3>
+            <h3 className="date">{day} {month} {date}, {year}</h3>
             <div className="clock">   
-                <Clock />
+                <Clock format={'hh-mm'} />
             </div>
         </div>
     )
