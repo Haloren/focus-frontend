24d2ba91dc46af22c189fe7ca5826ae07c646e05
Haloren <mckennakyler@gmail.com>
2021-01-21 20:21:10 -0700

    moved events & todos titles outside of container

--- src/containers/EventsContainer.js
+++ src/containers/EventsContainer.js
@@ -5,19 +5,20 @@ import Events from '../components/Events';
 class EventsContainer extends React.Component {
     render() {
         return(
-            <div className="events">
-                <div className="container-title">
+            <>
+                <div className="events-title">
                     Up Coming Events 
                     {/* <button className="expand-btn">+</button> */}
                 </div>
-                <hr></hr>
-                <div>
-                    <Events events={this.props.user && this.props.user.events} />
+                <div className="events">
+                    <div>
+                        <Events events={this.props.user && this.props.user.events} />
+                    </div>
+                    <div className="event-form">
+                        <EventForm user={this.props.user}/>
+                    </div>
                 </div>
-                <div className="event-form">
-                    <EventForm user={this.props.user}/>
-                </div>
-            </div>
+            </>
         )
     }
 }
--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -6,19 +6,20 @@ import Todos from '../components/Todos';
 class TodosContainer extends React.Component {
     render() {
         return(
-            <div className="todos">
-                <div className="container-title">
+            <>
+                <div className="todos-title">
                     Todo List 
                     {/* <button className="expand-btn">+</button> */}
                 </div>
-                <hr></hr>
-                <div>
-                    <Todos todos={this.props.user && this.props.user.todos} />
+                <div className="todos">
+                    <div>
+                        <Todos todos={this.props.user && this.props.user.todos} />
+                    </div>
+                    <div className="todos-form">
+                        <TodoForm user={this.props.user} />
+                    </div>
                 </div>
-                <div className="todos-form">
-                    <TodoForm user={this.props.user} />
-                </div>
-            </div>
+            </>
         )
     }
 }
